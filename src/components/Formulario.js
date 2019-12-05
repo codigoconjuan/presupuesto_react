f13a696b7974508fdd93644c2e450fa933f3dcda
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 14:03:07 -0600

    generando objeto de gasto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Error from './Error';
+import shortid from 'shortid';
 
 const Formulario = () => {
 
@@ -20,6 +21,13 @@ const Formulario = () => {
         guardarError(false);
 
         // construir el gasto
+        const gasto = {
+            nombre, 
+            cantidad, 
+            id: shortid.generate()
+        }
+
+        console.log(gasto);
 
         // pasar el gasto al componente principal
 
