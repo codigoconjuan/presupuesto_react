cec2313c42dc002f67d2c217e7f5277a5f4496af
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 12:53:46 -0600

    agregando state de presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    // definir el state
+    const [ cantidad, guardarCantidad ] = useState(0);
+
+
+    // Función que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad( parseInt(e.target.value, 10) )
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+
+
+        // si se pasa la validación
+    }
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input 
