ffcd7c8313a9e0cfe5e66f40a18526c36c2eb5e9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 13:14:38 -0600

    definiendo presupuesto y restante

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
 
 function App() {
+
+  // definir el state
+  const [ presupuesto, guardarPresupuesto] = useState(0);
+  const [ restante, guardarRestante] = useState(0);
+
+
   return (
     <div className="container">
         <header>
             <h1>Gasto Semanal</h1>
 
             <div className="contenido-principal contenido">
-                <Pregunta />
+                <Pregunta 
+                  guardarPresupuesto={guardarPresupuesto}
+                  guardarRestante={guardarRestante}
+                />
             </div>
         </header>
     </div>
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error';
 
-const Pregunta = () => {
+const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
 
     // definir el state
     const [ cantidad, guardarCantidad ] = useState(0);
@@ -24,6 +24,8 @@ const Pregunta = () => {
 
         // si se pasa la validación
         guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
     }
 
     return ( 
